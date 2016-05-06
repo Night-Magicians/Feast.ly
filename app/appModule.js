75ce0b7ec65a5ecfb20702e441daea990376ef9f
selfresonator <selfresonator@gmail.com>
2016-05-06 09:56:34 -0700

    Cleaned up unused files and folders, cleaned up some style.

--- app/appModule.js
+++ app/appModule.js
@@ -1,1 +1,9 @@
-var feastly = angular.module('feastly', ['feastly.recipe','feastly.home','feastly.landing', 'search.services', 'search.directive','feastly.services', 'landing.services', 'ui.router'] );
+var feastly = angular.module('feastly', [
+  'feastly.recipe',
+  'feastly.home',
+  'feastly.landing',
+  'search.services',
+  'search.directive',
+  'feastly.services',
+  'landing.services',
+  'ui.router'] );
