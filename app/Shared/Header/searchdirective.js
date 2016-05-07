9133d7e2657ec41c9a96e752dd74a8c9b7d17735
Aralya Phinith <aphinith84@gmail.com>
2016-05-07 12:15:39 -0700

    link to recipe page worked

--- app/Shared/Header/searchdirective.js
+++ app/Shared/Header/searchdirective.js
@@ -7,21 +7,21 @@ angular.module('search.directive', [])
   $scope.searchRecipes = function(input){
     input.toString();
     $scope.userInput['q'] = input.split(" ").join('+');
-    console.log('this is the query:', $scope.userInput)
+    // console.log('this is the query:', $scope.userInput)
     Recipes.getRecipes($scope.userInput).then(function(searchRecipe){
         console.log('searchRecipe:',searchRecipe)
          $scope.list = searchRecipe.data.matches;
           console.log('results inside searchdirective.js:', $scope.list)
 
-        return searchRecipe;
-        // Look into this later
-        if($location.current.url !== '/results'){
-          console.log("Inside if Statement", $location.current.url)
-          $location.path('/results');
-        } else {
-          console.log("no!!!!!!!!!!!")
-        $location.path($location.current, {}, {reload: true});          
-        }
+        // return searchRecipe;
+        // // Look into this later
+        // if($location.current.url !== '/results'){
+        //   // console.log("Inside if Statement", $location.current.url)
+        //   $location.path('/results');
+        // } else {
+        //   // console.log("no!!!!!!!!!!!")
+        // $location.path($location.current, {}, {reload: true});          
+        // }
 
         // return data;
         //Look into this later
