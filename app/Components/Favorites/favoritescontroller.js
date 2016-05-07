356a4fcf0fb237b91cc9183ef12e321bfbab6dd4
Dion <Dion.Fulwood@gmail.com>
2016-05-07 09:50:10 -0700

    Completed the favorites controller. Started initial setup of favoritesview.html

--- app/Components/Favorites/favoritescontroller.js
+++ app/Components/Favorites/favoritescontroller.js
@@ -1,10 +1,14 @@
 angular.module ('feastly.favorites', [])
 .controller('FavoritesController', function($scope) {
 
-$scope.favorites = {
+  $scope.favorites = {}
 
-}
+//to get recipes from back end
+  $scope.getFavs = function(){
+     Recipes.getFavorites().then(function(res){
+      $scope.favorites.data = res.data;
+     });
+  }
 
-
-
+$scope.getFavs();
 });
