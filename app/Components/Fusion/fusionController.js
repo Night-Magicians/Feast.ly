1541aa303554be820ff5dc63a61d9f72dee14d0a
selfresonator <selfresonator@gmail.com>
2016-05-09 12:24:17 -0700

    Removed console statements

--- app/Components/Fusion/fusionController.js
+++ app/Components/Fusion/fusionController.js
@@ -17,9 +17,7 @@ angular.module('feastly.fusion', [])
       var randIndex2 = Math.floor(Math.random() * $scope.searchList.length);
       $scope.fusionPair.push($scope.searchList[randIndex1]);
       $scope.fusionPair.push($scope.searchList[randIndex2]);
-      console.log($scope.fusionPair);
       var searchPair = $scope.fusionPair.join(' ');
-      console.log('searchPair',searchPair);
       return $http({
             method: 'GET',
             url: 'http://api.yummly.com/v1/api/recipes?_app_id=85328aaa&_app_key=9b9c3f69de268c05cd19da7b5bea7a42&q='+ searchPair + ''
