638c0f13ebb5a2a7576f3156b2e5fcf9b8ea538e
selfresonator <selfresonator@gmail.com>
2016-05-09 10:47:03 -0700

    Implemented client-side data grab for searchHistory

--- app/Components/Fusion/fusionController.js
+++ app/Components/Fusion/fusionController.js
@@ -1,5 +1,11 @@
 angular.module('feastly.fusion', [])
 
 .controller('FusionController', function($scope, Recipe, Recipes) {
+  $scope.searchList = [];
+  Recipes.getSearches()
+    .then(function(res) {
+      $scope.searchList = res.data;
+      console.log($scope.searchList);
+    });
   var randIndex = Math.floor(Math.random() * 100);
 });
