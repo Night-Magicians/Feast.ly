b5b0fe2b661fef2e2e6dda0e493f25a13b55e4ac
selfresonator <selfresonator@gmail.com>
2016-05-09 12:06:25 -0700

    Data now coming back to client side from FusionSearch

--- app/Components/Fusion/fusionController.js
+++ app/Components/Fusion/fusionController.js
@@ -1,9 +1,10 @@
 angular.module('feastly.fusion', [])
 
-.controller('FusionController', function($scope, Recipe, Recipes) {
+.controller('FusionController', function($scope, $http, Recipe, Recipes) {
   $scope.searchObj = {};
   $scope.searchList = [];
   $scope.fusionPair = [];
+  $scope.featured = {};
   Recipes.getSearches()
     .then(function(res) {
       $scope.searchObj = res.data;
@@ -16,6 +17,20 @@ angular.module('feastly.fusion', [])
       var randIndex2 = Math.floor(Math.random() * $scope.searchList.length);
       $scope.fusionPair.push($scope.searchList[randIndex1]);
       $scope.fusionPair.push($scope.searchList[randIndex2]);
+      console.log($scope.fusionPair);
+      var searchPair = $scope.fusionPair.join(' ');
+      console.log('searchPair',searchPair);
+    })
+    .then(function() {
+      return $http({
+        method: 'GET',
+        url: 'http://api.yummly.com/v1/api/recipes?_app_id=85328aaa&_app_key=9b9c3f69de268c05cd19da7b5bea7a42&q='+ $scope.searchPair + ''
+      }).then(function(res) {
+        $scope.featured = res.data;
+        console.log($scope.featured)
+        // return res.data;
+      });
     });
 
+  // $scope.doPairSearch = function() {};
 });
