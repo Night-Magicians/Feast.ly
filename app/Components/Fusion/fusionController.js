7a5d9b55feab6052d8a71d7a17d083b883641aa1
selfresonator <selfresonator@gmail.com>
2016-05-09 11:10:45 -0700

    Implemented client side searchList to use for Fusions.

--- app/Components/Fusion/fusionController.js
+++ app/Components/Fusion/fusionController.js
@@ -1,11 +1,20 @@
 angular.module('feastly.fusion', [])
 
 .controller('FusionController', function($scope, Recipe, Recipes) {
+  $scope.searchObj = {};
   $scope.searchList = [];
   Recipes.getSearches()
     .then(function(res) {
-      $scope.searchList = res.data;
+      $scope.searchObj = res.data;
+    })
+    .then(function() {
+      for (var i = 0; i < $scope.searchObj.length; i++) {
+        $scope.searchList.push($scope.searchObj[i].searchItem);
+      }
       console.log($scope.searchList);
     });
+
+
+  console.log('$scope.searchObj',$scope.searchObj);
   var randIndex = Math.floor(Math.random() * 100);
 });
