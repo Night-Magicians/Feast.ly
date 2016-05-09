5b0345541f8bb3a77aaf3e69d3ad8b02bf0f7ca4
selfresonator <selfresonator@gmail.com>
2016-05-09 11:26:47 -0700

    Created a list of random ingredients for Fusion feature

--- app/Components/Fusion/fusionController.js
+++ app/Components/Fusion/fusionController.js
@@ -3,6 +3,7 @@ angular.module('feastly.fusion', [])
 .controller('FusionController', function($scope, Recipe, Recipes) {
   $scope.searchObj = {};
   $scope.searchList = [];
+  $scope.fusionPair = [];
   Recipes.getSearches()
     .then(function(res) {
       $scope.searchObj = res.data;
@@ -11,10 +12,10 @@ angular.module('feastly.fusion', [])
       for (var i = 0; i < $scope.searchObj.length; i++) {
         $scope.searchList.push($scope.searchObj[i].searchItem);
       }
-      console.log($scope.searchList);
+      var randIndex1 = Math.floor(Math.random() * $scope.searchList.length);
+      var randIndex2 = Math.floor(Math.random() * $scope.searchList.length);
+      $scope.fusionPair.push($scope.searchList[randIndex1]);
+      $scope.fusionPair.push($scope.searchList[randIndex2]);
     });
 
-
-  console.log('$scope.searchObj',$scope.searchObj);
-  var randIndex = Math.floor(Math.random() * 100);
 });
